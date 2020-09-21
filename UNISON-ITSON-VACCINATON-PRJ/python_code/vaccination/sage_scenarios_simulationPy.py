a2b5d97b0191d8fd70e249403079a2705f8ba276
saul <sauldiazinfante@gmail.com>
2020-09-20 21:40:10 -0700

    Commit with new path folder structure.

--- UNISON-ITSON-VACCINATON-PRJ/python_code/vaccination/sage_scenarios_simulationPy.py
+++ UNISON-ITSON-VACCINATON-PRJ/python_code/vaccination/sage_scenarios_simulationPy.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 from sage_scenarios_simulation import *
 
-bocop_main_scene_data = 'bocop_data/' + 'deltaR_05_Coverage_50_TH_10M_14M/' \
+bocop_main_scene_data = './bocop_data/' + 'deltaR_05_Coverage_50_TH_10M_14M/' \
                         + 'delta_V_05_years/' \
                         + 'epsilon-7.000000e-01.sol'
-                        # + 'delta_V_lifelong/'
-                        # + 'deltaR_05_Coverage_50_TH_10M_14M'
-                        # +'/delta_V_05_years/'
-                        # + 'epsilon-7.000000e-01.sol'
+# + 'delta_V_lifelong/'
+# + 'deltaR_05_Coverage_50_TH_10M_14M'
+# +'/delta_V_05_years/'
+# + 'epsilon-7.000000e-01.sol'
 
 
 scm = CovidModels(bocop_solution_file=bocop_main_scene_data)
